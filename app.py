b52c969f46e48d513b902a68388ac15a763d763a
Ankur Kumar <kumarankur0006@gmail.com>
2025-01-05 18:34:05 +0530

    Third Experimnt CI/CD

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ async def predictRouteClient(request: Request):
 if __name__ == "__main__":
     app_run(app, host=APP_HOST, port=APP_PORT)
 
-# Now my app is ready
+# Now my app is ready to used 
